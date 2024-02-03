e777596373bd3e3c1af55b83c86dcbced3b28d27
krutyosila <ali@soyturk.com>
2024-02-03 14:10:57 +0300

    admin login

--- resources/views/vendor/adminhub/components/branding/logo.blade.php
+++ resources/views/vendor/adminhub/components/branding/logo.blade.php
@@ -1,3 +1,3 @@
 <div style="text-align: center">
-<img src="{{ asset('img/logo.png') }}" style="height: 24px" alt="">
+<img src="{{ asset('img/logo.png') }}" style="height: 24px; margin: 0 auto" alt="">
 </div>
