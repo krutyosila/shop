92e0d3ae481066292a7ca251ac46f99547614ca5
krutyosila <ali@soyturk.com>
2024-02-06 02:38:16 +0300

    product list, thumb border

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -2,7 +2,7 @@
 
 <a class="block group"
    href="{{ route('product.view', $product->defaultUrl->slug) }}">
-    <div class="overflow-hidden rounded-lg aspect-w-1 aspect-h-1">
+    <div class="overflow-hidden border rounded-lg aspect-w-1 aspect-h-1">
         @if ($product->thumbnail)
             <img class="object-cover transition-transform duration-300 group-hover:scale-105"
                  src="{{ $product->thumbnail->getUrl('medium') }}?v=1"
