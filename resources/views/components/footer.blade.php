cd10af55d69ff742521688fa9c389fa7d7b19fa9
krutyosila <ali@soyturk.com>
2024-02-15 16:06:47 +0300

    footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,13 +1,63 @@
 <footer class="bg-gray-50">
     <div class="max-w-screen-xl px-4 py-12 mx-auto sm:px-6 lg:px-8">
-        <x-brand.logo class="w-auto h-8 text-indigo-600" />
-
-        <p class="max-w-sm mt-4 text-gray-700">
-            ...
-        </p>
-
+        <div class="d-lg-flex align-items-center justify-between text-center">
+            <x-brand.logo class="w-auto h-8 text-indigo-600"/>
+            <img src="{{ asset('img/paymethods.png') }}" style="width: 100%; max-width: 420px;" alt=""
+                class="mt-4 mt-md-0">
+        </div>
+        <div class="small text-muted" style="text-align: justify;">
+            <p class="max-w-sm mt-4 text-gray-700">
+                <b>WHY HGHROID TO BUY ANABOLIC STEROIDS?</b>
+            <p>
+                Anabolic steroids are sold everywhere now but why should we be very careful when purchasing these
+                products?
+                Because our customers who contact us for the first time usually say the following; I bought anabolic
+                steroid
+                but I couldn’t grow because it’s fake.. yes, there are hundreds of fake anabolic steroid and human
+                growth
+                hormone products.
+            </p>
+            <b>WHO ARE WE?</b>
+            <p>
+                We, as HGHROID, have been providing quality and 100% original anabolic steroid, growth hormones and
+                peptides
+                to people for 3 years. The reason for these discounted prices we give to people is that we send our
+                products
+                ourselves from our warehouse, not dropshipping. (Don’t forget to email us for trust references.)
+            </p>
+            <b>BE CAREFUL WHEN BUYING ANABOLIC STEROIDS ONLINE WHY?</b>
+            <p>
+                Be careful when purchasing anabolic steroid and hormones from websites because dozens of sites will not
+                take
+                your money and send you products. The scammers who say that they will give you these products at a very
+                affordable price will steal you. That’s why you can choose us to buy steroid. because we have good
+                reviews
+                and great references on trusted forum sites.
+            </p>
+            <b>OUR WAREHOUSES</b>
+            <p>
+                I have 3 different warehouses as Hghroid International shipping, eu domestic and us domestic.
+            </p>
+            <p>
+                If you are looking for a place where you can buy steroids online USA, you are in the right place. In
+                this
+                way, we try to provide you with the best service we can and we are part of forum eroids to give you
+                confidence.
+            </p>
+            <p>
+                You can safely order from our international warehouses to anywhere in the world.
+            </p>
+            <p>
+                You can safely order from any country in Europe for Eu shipping.
+            </p>
+            <p>
+                info; Don’t forget to consult your doctor before using anabolic steroids and hormones!
+            </p>
+        </div>
         <p class="pt-4 mt-4 text-sm text-gray-500 border-t border-gray-100">
-            &copy; {{ now()->year }} HGHROID
+
+            Copyright © 2019-&copy; {{ now()->year }} HghRoid.com © All Rights Reserved
         </p>
+
     </div>
 </footer>
