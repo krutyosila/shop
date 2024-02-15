8d3f64ff5f33fb52eb86f0fd9384174a6f32cf30
krutyosila <ali@soyturk.com>
2024-02-15 14:54:12 +0300

    slide and faq

--- resources/views/components/welcome-banner.blade.php
+++ resources/views/components/welcome-banner.blade.php
@@ -1,13 +1,12 @@
 <div id="carouselExampleRide" class="carousel slide" data-bs-ride="true">
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <img src="data:image/gif;base64,R0lGODlhAQABAIAAAMLCwgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" class="d-block w-100" alt="" style="height: 480px">
-        </div>
-        <div class="carousel-item">
-            <img src="data:image/gif;base64,R0lGODlhAQABAIAAAMLCwgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" class="d-block w-100" alt="" style="height: 480px">
-        </div>
-        <div class="carousel-item">
-            <img src="data:image/gif;base64,R0lGODlhAQABAIAAAMLCwgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" class="d-block w-100" alt="" style="height: 480px">
+            <div class="d-none d-md-block">
+                <img src="data:image/gif;base64,R0lGODlhAQABAIAAAMLCwgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" class="d-block w-100" alt="" style="height: 480px">
+            </div>
+            <div class="d-md-none">
+                <img src="{{ asset('img/img.png')  }}" class="d-block w-100" alt="">
+            </div>
         </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="prev">
---
+++ resources/views/livewire/faq.blade.php
@@ -0,0 +1,88 @@
+<div class="container py-3">
+
+
+<p><strong>FAQ:</strong><br>
+        <strong>1) How can I make order in your shop?</strong><br>
+        You must choose interesting products, choose quantity, then press ADD TO CART, after you must press VIEW CART
+        and you will come to Checkout menu. There you must enter BILLING &amp; SHIPPING information (type there your
+        address for delivery: your name, country, city, postal/zip code and your valid email address. Email address
+        important, we will contact to you there), then choose your way of payment (MoneyGram, Western Union or Bitcoin)
+        and press PLACE ORDER. After you place your order on your email will come automatic message with your order. Do
+        not needed to ask us where you will find info for payment, after we will receive your order, in short time we
+        will send you information of payment (BTC address or info for WU/MG transfer).<br>
+        Please make sure you enter correct address for delivery and name. We can’t help you if order will come on wrong
+        address.</p>
+    <p><strong>2) How do you ship your orders?</strong><br>
+        We have 2 warehouses – first is International (worldwide), Airmail shipping. Second warehouse – USA Domestic,
+        shipping from inside USA and only to USA addresses, shipping with USPS priority 2 days.<br>
+        INTERNATIONAL SHIPPING (Shipping to all countries, 100% delivery guaranty to USA)<br>
+        Products in Stock: Balkan Pharmaceuticals, SP Laboratories, Zhengzhou Pharmaceutical (ZPHC) and&nbsp;Spectrum
+        Pharma<br>
+        EU DOMESTIC SHIPPING:<br>
+        Products in Stock: MAXLAB</p>
+    <p><strong>3) How much delivery will cost аfor me?</strong><br>
+        Airmail shipping (Worldwide)/USPS priority shipping (USA Domestic)<br>
+        Price:&nbsp;$30<br>
+        Order’s sent from International warehouse (100% delivery guaranty to USA) or USA Domestic warehouse.</p>
+    <p><strong>4) How long is the delivery time after payment received?</strong><br>
+        In general, delivery times are:<br>
+        Regular Postal Shipping:<br>
+        Worldwide: 10-31 days (average 12-20 days). Shipping time up to 10 days. Tracking numbers for International 2-3
+        days after shipping.<br>
+        USPS shipping (USA domestic orders):<br>
+        US Warehouse : 3-6 business days. Shipping every 2-3d day, usps priority 2 days.<br>
+        EU DOMESTIC: 3-6 business days. Shipping every 2-3d day</p>
+    <p><strong>5) Signature required for orders in USA? Do you ship to PO boxes?</strong><br>
+        US Domestic orders – no, signature do not needed! Courier will drop your parcel at your address.<br>
+        Yes, we ship to PO boxes from US Domestic stock.<br>
+        International orders to USA: mailman will bring your parcel to your address, if you will be out of home or you
+        will not open your door or something else he will leave you notification, then you must to go USPS office and
+        take your parcel (also you can ask for redelivery once from USPS website). All orders has discreet packaging,
+        you can not scare to go in USPS office and pick up your parcel if you miss mailman. If parcel passing customs
+        and come with USPS to you, it can’t be any problems. International orders can be also shipped to PO boxes.</p>
+    <p><strong>6) Do you send tracking number after shipping ?</strong><br>
+        Yes, we always send tracking numbers, does not matter from where shipping is International or USA Domestic, you
+        will always have your tracking number.<br>
+        International tracking numbers 2-3 days after shipping.</p>
+    <p><strong>7) Do you reship seized/lost/broken orders?</strong><br>
+        Yes, we reship seized packages 100% once for free. we have 100% guaranteed shipping to USA.<br>
+        If your order will be seized you need to send us the seizure letter, if seizure do not understandable from the
+        tracking status. After seizure letter we will reship your order for free.<br>
+        We do not reship to the following high risk countries: Australia,Canada, New Zealand, Peru, Brazil, Israel,
+        Scandinavian Countries and Finland.<br>
+        If you have strict customs, bad passing rate to your country, regular seizures – you must tell about it before
+        ordering or ask us about rates to your country.<br>
+        We can not control if USPS for example will lost your parcel, we do not reship lost parcels or parcels shipped
+        on not correct address given by customer. We will give you 100% guarantee we sent your order correct to the
+        address given for us in your order, there can not be mistakes from our side.<br>
+        We will reship for free broken/missed items only in case if you will send for us video with unpacking your
+        parcel .</p>
+    <p><strong>8) How can I pay my order?</strong><br>
+        You can pay with MoneyGram, Western Union or Bitcoin. After you place order we will give information for
+        payments of method you chose. If you chose MoneyGram or Western Union, we will send via email info of transfer
+        receiver, it takes to pick up your money (1-3 business days). &nbsp;Bitcoin best one to pay fast and safe, after
+        you choosing bitcoin as payment method, we will send you via email our bitcoin wallet.</p>
+    <p>To send bitcoins use Cash App service, it is really easy to use. (if you first time using CashApp you need to
+        make verification, next all your transaction will be instant). Video instruction for Cash App how to transfer
+        bitcoins – https://youtu.be/FoeSOe31lcI<br>
+        Or Paybis service, it is also really easy to use with your credit card. Fast verification, easy to navigate.
+        Make sure you chose correct bitcoin there – BTC (not BCH for example, BCH is bitcoin cash, it is different
+        cryptocurrency). Video instruction for Paybis how to transfer bitcoins –
+        https://www.youtube.com/watch?v=hhg_pvk4w5E<br>
+        Also you can use Moonpay. You can pay there even with Apple Pay. Verification take only about 30 minutes and
+        needed only first time.<br>
+        Coinbase as variant too. But if you make first transfer there, they can block it till 7 days for security.<br>
+        IF YOU WILL TRY ONE TIME BITCOIN, YOU WILL NEVER WANT TO USE SOMETHING ELSE. IT IS STEALTH, INSTANT AND
+        SECURE!!! JUST TRY IT ONE MORE TIME AND YOU WILL SAVE 30 TIMES MORE YOUR TIME IN FUTURE!</p>
+    <p><strong>9) Can I pay MoneyGram or Western Union online or I should go to the store?</strong><br>
+        You can send transfer online, but please know it may have big risks of transfer blocking. MG and WU fighting
+        against carding, so they can block your transfer for your security. So best one way you must go to the store and
+        send your transfer there.</p>
+    <p><strong>10) Can I mix products from USA Domestic warehouse and International warehouse?</strong><br>
+        Yes, you can. But you will receive 2 orders from different warehouses. You will pay only one time for shipping
+        30$, second delivery will be for free.</p>
+    <p><strong>11) How can I know you are legit and you will send my order without scam?</strong><br>
+        We are official distributors of ZPHC and Spectrum Pharma and long time already in this business. We have perfect
+        reputation, all our customers always satisfied. We are represented on biggest boards and have 0 bad reviews or
+        feedbacks about our services and products, you can easy check it personally and read about our services.</p>
+</div>
