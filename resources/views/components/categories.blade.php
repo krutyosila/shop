fb6702f5ac457da4f643369a11017d6d952611f2
krutyosila <ali@soyturk.com>
2024-02-17 13:31:03 +0300

    home and footer

--- resources/views/components/categories.blade.php
+++ resources/views/components/categories.blade.php
@@ -1,16 +1,16 @@
 <div class="grid grid-cols-2 mt-8 lg:grid-cols-3 gap-x-4 gap-y-8">
     <div class="block rounded overflow-hidden">
         <img src="{{ asset('img/banner_1.png') }}"
-             class="d-block border rounded w-100" alt="" style="height: 180px">
+             class="d-block border rounded w-100" alt="" style="max-height: 180px">
     </div>
     <div class="block rounded overflow-hidden">
         <img src="{{ asset('img/banner_3.jpeg') }}"
-             class="d-block border rounded w-100" alt="" style="height: 180px">
+             class="d-block border rounded w-100" alt="" style="max-height: 180px">
     </div>
 
     <div class="block rounded overflow-hidden">
         <img src="{{ asset('img/banner_5.jpeg') }}"
-             class="d-block border rounded w-100" alt="" style="height: 180px">
+             class="d-block border rounded w-100" alt="" style="max-height: 180px">
     </div>
     <!--
     @foreach ($this->brands as $brand)
