ce2891b47d5350c1c1277fbad07508eb976880cc
krutyosila <ali@soyturk.com>
2024-02-17 15:01:32 +0300

    payment

---
+++ resources/views/livewire/payment.blade.php
@@ -0,0 +1,42 @@
+<div class="container py-5">
+    <h2 class="h4">Ordering and Payment</h2>
+
+    <p>
+        Register with us first. Pick your items using the shopping cart. Submit your order. You will receive an email with payment instructions. Email us if you don't receive your payment instructions within 24 hours.
+    </p>
+    <p>
+        Our payment methods are absolutely secure and anonymous. We accept Western Union and Moneygram and also payments with Bitcoin.
+    </p>
+    <div class="d-flex justify-between align-items-center py-3">
+        <img src="{{ asset('img/bitcoin_transfer.png') }}" alt="" style="max-width: 100px;">
+        <p class="ps-4 w-100">
+        <strong>Bitcoin</strong><br>
+        Save 10% on your order by simply choosing Bitcoin at checkout. No promo code required please click here for detailed information on Bitcoin https://bitcoin.org/en/getting-started
+        </p>
+    </div>
+
+    <div class="d-flex justify-between align-items-center py-3">
+        <img src="{{ asset('img/western_transfer.png') }}" alt="" style="max-width: 100px;">
+        <p class="ps-4 w-100">
+            <strong>Western Union</strong><br>
+            Can be sent from nearest agent location or online with your credit card.
+        </p>
+    </div>
+
+
+    <div class="d-flex justify-between align-items-center py-3">
+        <img src="{{ asset('img/moneygram_transfer.png') }}" alt="" style="max-width: 100px;">
+        <p class="ps-4 w-100">
+            <strong>Money Gram</strong><br>
+            Can be sent from nearest agent location or online with your credit card.
+        </p>
+    </div>
+
+    <p>
+        All necessary information and payment instructions will be send to you after your order confirmation. Please check your order confirmation email and follow the instructions carefully.
+    </p>
+    <p>
+        If you need more information on Payment please contact us.
+
+    </p>
+</div>
