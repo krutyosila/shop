745a3d0767b581bc0a4ac2171ab74de04457e52f
krutyosila <ali@soyturk.com>
2024-02-17 13:10:59 +0300

    home and footer

--- resources/views/components/categories.blade.php
+++ resources/views/components/categories.blade.php
@@ -1,7 +1,23 @@
-<div class="grid grid-cols-2 mt-8 lg:grid-cols-4 gap-x-4 gap-y-8">
+<div class="grid grid-cols-2 mt-8 lg:grid-cols-3 gap-x-4 gap-y-8">
+    <div class="block rounded overflow-hidden">
+        <img src="{{ asset('img/banner_1.png') }}"
+             class="d-block border rounded w-100" alt="" style="height: 180px">
+    </div>
+    <div class="block rounded overflow-hidden">
+        <img src="{{ asset('img/banner_3.jpeg') }}"
+             class="d-block border rounded w-100" alt="" style="height: 180px">
+    </div>
+
+    <div class="block rounded overflow-hidden">
+        <img src="{{ asset('img/banner_5.jpeg') }}"
+             class="d-block border rounded w-100" alt="" style="height: 180px">
+    </div>
+    <!--
     @foreach ($this->brands as $brand)
-        <div class="block rounded overflow-hidden"><img
-                src="data:image/gif;base64,R0lGODlhAQABAIAAAMLCwgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="
-                class="d-block w-100" alt="" style="height: 180px"></div>
+        <div class="block rounded overflow-hidden">
+                <img src="data:image/gif;base64,R0lGODlhAQABAIAAAMLCwgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="
+                     class="d-block w-100" alt="" style="height: 180px">
+        </div>
     @endforeach
+    -->
 </div>
