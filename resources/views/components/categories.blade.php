75ad2cf5a60a4c64c0b05c113b9f07062f68207f
krutyosila <ali@soyturk.com>
2024-02-19 00:02:10 +0300

    brand

--- resources/views/components/categories.blade.php
+++ resources/views/components/categories.blade.php
@@ -1,16 +1,15 @@
 <div class="grid grid-cols-2 mt-8 lg:grid-cols-4 gap-x-4 gap-y-8">
     @foreach ($this->brands as $brand)
+        @if($brand->getMedia('images')->count() > 0)
         <a href="{{ route('brands.view', ['slug' => $brand->defaultUrl->slug]) }}" class="block rounded overflow-hidden">
-            @if($brand->getMedia('images')->count() > 0)
+
                 @php
                 $media = $brand->getMedia('images');
                 @endphp
                 <img src="{{$media[0]->getFullUrl()}}"
                      class="d-block w-100" alt="" style="height: 180px">
-            @else
-                <img src="data:image/gif;base64,R0lGODlhAQABAIAAAMLCwgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="
-                     class="d-block w-100" alt="" style="height: 180px">
-            @endif
+
         </a>
+        @endif
     @endforeach
 </div>
