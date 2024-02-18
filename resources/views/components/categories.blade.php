519e67d41594823fa146d9e6391e50dc56db7beb
krutyosila <ali@soyturk.com>
2024-02-18 23:56:46 +0300

    brand

--- resources/views/components/categories.blade.php
+++ resources/views/components/categories.blade.php
@@ -1,28 +1,16 @@
 <div class="grid grid-cols-2 mt-8 lg:grid-cols-4 gap-x-4 gap-y-8">
-    <div class="block rounded overflow-hidden">
-        <img src="{{ asset('img/banner_1.png') }}"
-             class="d-block border rounded w-100" alt="" style="max-height: 180px">
-    </div>
-    <div class="block rounded overflow-hidden">
-        <img src="{{ asset('img/banner_3.jpeg') }}"
-             class="d-block border rounded w-100" alt="" style="max-height: 180px">
-    </div>
-
-    <div class="block rounded overflow-hidden">
-        <img src="{{ asset('img/banner_5.jpeg') }}"
-             class="d-block border rounded w-100" alt="" style="max-height: 180px">
-    </div>
-
-    <div class="block rounded overflow-hidden">
-        <img src="{{ asset('img/banner_6.jpeg') }}"
-             class="d-block border rounded w-100" alt="" style="max-height: 180px">
-    </div>
-    <!--
     @foreach ($this->brands as $brand)
-        <div class="block rounded overflow-hidden">
+        <a href="{{ route('brands.view', ['slug' => $brand->defaultUrl->slug]) }}" class="block rounded overflow-hidden">
+            @if($brand->getMedia('images')->count() > 0)
+                @php
+                $media = $brand->getMedia('images');
+                @endphp
+                <img src="{{$media[0]->getFullUrl()}}"
+                     class="d-block w-100" alt="" style="height: 180px">
+            @else
                 <img src="data:image/gif;base64,R0lGODlhAQABAIAAAMLCwgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="
                      class="d-block w-100" alt="" style="height: 180px">
-        </div>
+            @endif
+        </a>
     @endforeach
-    -->
 </div>
