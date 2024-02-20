b9a6beb7a40a3f4e92a355be0910c68dc50f7f77
krutyosila <ali@soyturk.com>
2024-02-20 15:13:36 +0300

    logo

--- resources/views/components/brand/logo.blade.php
+++ resources/views/components/brand/logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ asset('img/logo.png') }}" style="height: 24px; max-width: none!important;" alt="">
+<img src="{{ asset('img/logo.png') }}" style="height: 30px; max-width: none!important;" alt="HGHRoid">
