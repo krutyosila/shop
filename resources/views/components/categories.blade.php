8c6e7f5b30a1daa5ea390c7c4c1dce35bd651df5
krutyosila <ali@soyturk.com>
2024-02-23 16:14:01 +0300

    slider

--- resources/views/components/categories.blade.php
+++ resources/views/components/categories.blade.php
@@ -3,7 +3,7 @@
         <img src="{{ asset('img/slider/web-banner-2.jpg') }}" class="d-block w-100" alt="" style="height: 480px">
     </div>
     <div class="d-md-none">
-        <img src="{{ asset('img/slider/mobile-banner-2-1.jpg')  }}" class="d-block w-100" alt="">
+        <img src="{{ asset('img/slider/mobile-banner-3.jpg')  }}" class="d-block w-100" alt="">
     </div>
 </div>
 <div class="grid grid-cols-2 mt-8 lg:grid-cols-4 gap-x-4 gap-y-8">
