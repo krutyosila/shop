0b5ba773e2657e162a266823f8be15ac592f909e
krutyosila <ali@soyturk.com>
2024-02-23 16:11:46 +0300

    slider

--- resources/views/components/categories.blade.php
+++ resources/views/components/categories.blade.php
@@ -1,3 +1,11 @@
+<div class="rounded overflow-hidden">
+    <div class="d-none d-md-block">
+        <img src="{{ asset('img/slider/web-banner-2.jpg') }}" class="d-block w-100" alt="" style="height: 480px">
+    </div>
+    <div class="d-md-none">
+        <img src="{{ asset('img/slider/mobile-banner-2-1.jpg')  }}" class="d-block w-100" alt="">
+    </div>
+</div>
 <div class="grid grid-cols-2 mt-8 lg:grid-cols-4 gap-x-4 gap-y-8">
     @foreach ($this->brands as $brand)
         @if($brand->getMedia('images')->count() > 0)
--- resources/views/components/welcome-banner.blade.php
+++ resources/views/components/welcome-banner.blade.php
@@ -8,14 +8,6 @@
                 <img src="{{ asset('img/slider/mobile-banner-1.jpg')  }}" class="d-block w-100" alt="">
             </div>
         </div>
-        <div class="carousel-item">
-            <div class="d-none d-md-block">
-                <img src="{{ asset('img/slider/web-banner-2.jpg') }}" class="d-block w-100" alt="" style="height: 480px">
-            </div>
-            <div class="d-md-none">
-                <img src="{{ asset('img/slider/mobile-banner-2.jpg')  }}" class="d-block w-100" alt="">
-            </div>
-        </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
